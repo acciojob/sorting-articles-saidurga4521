d5f7bbb81fae51b52ab08a28ff9637f1f6681c3f
sai kamireddi <111673552+saidurga4521@users.noreply.github.com>
2025-05-23 15:08:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your JS code here. If required.
-const ulE1=document.getElementById('band');
+const ulE1=document.getElementById('bands');
 const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
 let res=[]
 const map=new Map()
